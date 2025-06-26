74834034f09d5244b50588f23efa77055e42e919
Svyatoslav Krivosheev <krivosheev.sviat@gmail.com>
2025-06-26 21:21:34 +0300

    fix code highlightning

--- src/gitbok/core.clj
+++ src/gitbok/core.clj
@@ -90,8 +90,19 @@
          (find-children-files context filepath)]
      (big-links/big-link-view (str "/" uri) title))])
 
-(defn render-file* [context filepath parsed title]
+(defn render-file* [context filepath parsed title raw-content]
   [:div {:class "flex-1 min-w-0 max-w-4xl"}
+   (when (re-find #"```" raw-content)
+     [:div
+      [:link {:rel "stylesheet" :href "/static/github.min.css"}]
+      [:script {:src "/static/highlight.min.js"}]
+      [:script {:src "/static/json.min.js"}]
+      [:script {:src "/static/bash.min.js"}]
+      [:script {:src "/static/yaml.min.js"}]
+      [:script {:src "/static/json.min.js"}]
+      [:script {:src "/static/http.min.js"}]
+      [:script {:src "/static/graphql.min.js"}]
+      [:script "hljs.highlightAll();"]])
    (if (= 1 (count (:content parsed)))
      (render-empty-page context filepath title)
      (markdown/render-md context filepath parsed))])
@@ -108,7 +119,7 @@
         {:keys [parsed description title]}
         (markdown/parse-markdown-content context [filepath content*])]
     (try
-      {:content (render-file* context filepath parsed title)
+      {:content (render-file* context filepath parsed title content*)
        :description (or description
                         (if (>= (count content*) 150)
                           (subs content* 0 150)
@@ -280,7 +291,6 @@
 
 (defn content-div [context uri content]
   [:main#content {:class "flex-1 py-6 max-w-6xl min-w-0 overflow-x-hidden"}
-   [:script "hljs.highlightAll();"]
    [:script "window.scrollTo(0, 0);"]
    [:div {:class "mx-auto px-2 max-w-full"} content]
    (navigation-buttons context uri)])
@@ -340,16 +350,6 @@
     [:script {:src "/static/toc.js"}]
     [:script {:src "/static/mobile-menu.js"}]
     [:script {:src "/static/keyboard-navigation.js"}]
-    [:link {:rel "stylesheet" :href "/static/github.min.css"}]
-    [:script {:src "/static/highlight.min.js"}]
-    [:script {:src "/static/highlightjs-line-numbers.min.js"}]
-    [:script {:src "/static/json.min.js"}]
-    [:script {:src "/static/bash.min.js"}]
-    [:script {:src "/static/yaml.min.js"}]
-    [:script {:src "/static/json.min.js"}]
-    [:script {:src "/static/http.min.js"}]
-    [:script {:src "/static/graphql.min.js"}]
-    [:script "hljs.highlightAll();"]
     [:script
      (uui/raw
       "!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(\".\");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement(\"script\")).type=\"text/javascript\",p.crossOrigin=\"anonymous\",p.async=!0,p.src=s.api_host.replace(\".i.posthog.com\",\"-assets.i.posthog.com\")+\"/static/array.js\",(r=t.getElementsByTagName(\"script\")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a=\"posthog\",u.people=u.people||[],u.toString=function(t){var e=\"posthog\";return\"posthog\"!==a&&(e+=\".\"+a),t||(e+=\" (stub)\"),e},u.people.toString=function(){return u.toString(1)+\".people (stub)\"},o=\"init Ie Ts Ms Ee Es Rs capture Ge calculateEventProperties Os register register_once register_for_session unregister unregister_for_session js getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty Ds Fs createPersonProfile Ls Ps opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing Cs debug I As getPageViewId captureTraceFeedback captureTraceMetric\".split(\" \"),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
