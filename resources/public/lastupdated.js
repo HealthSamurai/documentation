35534178059898206a629c477e97994ed20159be
Svyatoslav Krivosheev <krivosheev.sviat@gmail.com>
2025-06-27 10:54:55 +0300

    fix lastupdated

---
+++ resources/public/lastupdated.js
@@ -0,0 +1,44 @@
+function formatRelativeTime(date) {
+  const now = new Date();
+  const then = new Date(date);
+  const diffMs = now - then;
+  const seconds = Math.floor(diffMs / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  let value, unit;
+  if (years > 0) {
+    value = -years; unit = "year";
+  } else if (months > 0) {
+    value = -months; unit = "month";
+  } else if (weeks > 0) {
+    value = -weeks; unit = "week";
+  } else if (days > 0) {
+    value = -days; unit = "day";
+  } else if (hours > 0) {
+    value = -hours; unit = "hour";
+  } else if (minutes > 0) {
+    value = -minutes; unit = "minute";
+  } else {
+    value = -seconds; unit = "second";
+  }
+
+  const rtf = new Intl.RelativeTimeFormat('en', { numeric: "auto" });
+  return rtf.format(value, unit);
+}
+
+function updateLastUpdated()
+{
+  const el = document.getElementById("lastupdated");
+  const isoTime = el.getAttribute("data-updated-at");
+  const relative = formatRelativeTime(isoTime);
+  el.textContent = "Last updated " + relative;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  updateLastUpdated();
+})
--- src/gitbok/core.clj
+++ src/gitbok/core.clj
@@ -287,14 +287,17 @@
 
 (defn content-div [context uri content filepath]
   [:main#content {:class "flex-1 py-6 max-w-6xl min-w-0 overflow-x-hidden"}
-   [:script "window.scrollTo(0, 0);"]
+   [:script "window.scrollTo(0, 0); updateLastUpdated();"]
    [:div {:class "mx-auto px-2 max-w-full"} content]
    (navigation-buttons context uri)
-   [:p {:class "mt-4 text-gray-600"}
-    "Last updated "
-    (get
-     (system/get-system-state context [const/LASTMOD])
-     filepath)]])
+   (let [lastupdated
+         (get
+          (system/get-system-state context [const/LASTMOD])
+          filepath)]
+    [:p {:class "mt-4 text-gray-600"
+         :id "lastupdated"
+         :data-updated-at lastupdated}
+     "Last updated " lastupdated])])
 
 (defn get-toc [context filepath]
   (let [rendered (get-rendered context filepath)]
@@ -351,6 +354,7 @@
     [:script {:src "/static/toc.js"}]
     [:script {:src "/static/mobile-menu.js"}]
     [:script {:src "/static/keyboard-navigation.js"}]
+    [:script {:src "/static/lastupdated.js"}]
     [:script
      (uui/raw
       "!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(\".\");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement(\"script\")).type=\"text/javascript\",p.crossOrigin=\"anonymous\",p.async=!0,p.src=s.api_host.replace(\".i.posthog.com\",\"-assets.i.posthog.com\")+\"/static/array.js\",(r=t.getElementsByTagName(\"script\")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a=\"posthog\",u.people=u.people||[],u.toString=function(t){var e=\"posthog\";return\"posthog\"!==a&&(e+=\".\"+a),t||(e+=\" (stub)\"),e},u.people.toString=function(){return u.toString(1)+\".people (stub)\"},o=\"init Ie Ts Ms Ee Es Rs capture Ge calculateEventProperties Os register register_once register_for_session unregister unregister_for_session js getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty Ds Fs createPersonProfile Ls Ps opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing Cs debug I As getPageViewId captureTraceFeedback captureTraceMetric\".split(\" \"),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
@@ -360,7 +364,7 @@
                                                                         person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
                                                                         })")]]
    [:body {:hx-boost "true"
-           :hx-on "htmx:afterSwap: window.scrollTo(0, 0); hljs.highlightAll();"}
+           :hx-on "htmx:afterSwap: window.scrollTo(0, 0); hljs.highlightAll(); updateLastUpdated()"}
     body]])
 
 (defn layout [context request
