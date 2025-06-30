9d1ff7e470bc6fd09f5c671bdd35797e284888bb
spicyfalafel <285574@niuitmo.ru>
2025-06-30 18:20:16 +0300

    fix generate og

--- scripts/generate-og.sh
+++ scripts/generate-og.sh
@@ -14,10 +14,20 @@ find docs -name '*.md' | while read -r mdfile; do
 
   echo "Creating preview: $pngpath ← \"$title\""
 
+if command -v magick >/dev/null 2>&1; then
   magick -size 1200x630 xc:white \
     \( .gitbook/assets/aidbox-logo.svg -resize 150x150 \) -gravity northeast -geometry +30+30 -composite \
     -gravity center -pointsize 40 -font fonts/Roboto-Regular.ttf -fill "#555555" -annotate +0+100 "Aidbox User Docs" \
     -gravity center -pointsize 80 -font fonts/Roboto-Regular.ttf -fill "#222222" -size 1000x200 -background none caption:"$title" \
     -gravity center -geometry +0-40 -composite \
     "$pngpath"
+else
+  convert -size 1200x630 xc:white \
+    \( .gitbook/assets/aidbox-logo.svg -resize 150x150 \) -gravity northeast -geometry +30+30 -composite \
+    -gravity center -pointsize 40 -font fonts/Roboto-Regular.ttf -fill "#555555" -annotate +0+100 "Aidbox User Docs" \
+    -gravity center -pointsize 80 -font fonts/Roboto-Regular.ttf -fill "#222222" -size 1000x200 -background none caption:"$title" \
+    -gravity center -geometry +0-40 -composite \
+    "$pngpath"
+fi
+
 done
