df6ec5acb0a25f8e4219195fc1cbc1a889c5d868
spicyfalafel <285574@niuitmo.ru>
2025-07-07 18:22:15 +0300

    send 404 to posthog

--- src/gitbok/ui/not_found.clj
+++ src/gitbok/ui/not_found.clj
@@ -3,12 +3,24 @@
    [gitbok.indexing.impl.file-to-uri :as file-to-uri]
    [gitbok.utils :as utils]
    [clojure.string :as str]
+   [uui]
    [gitbok.search]))
 
 (defn not-found-view [context uri]
   (let [search-term (last (str/split uri #"/"))
         search-results (gitbok.search/search context search-term)]
     [:div.min-h-screen.flex.items-center.justify-center
+     [:script
+      (uui/raw "
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.posthog && posthog.capture) {
+    posthog.capture('Page Not Found', {
+      path: window.location.pathname,
+      full_url: window.location.href,
+      referrer: document.referrer,
+    });
+  }
+});")]
      [:div.max-w-2xl.w-full.px-4
       [:div
        [:h2.mt-4.text-3xl.font-semibold.text-gray-700.text-center "Page not found"]
