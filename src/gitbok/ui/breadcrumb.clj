d3e641319c4581e7b84941334a0660c018d4a469
Svyatoslav Krivosheev <krivosheev.sviat@gmail.com>
2025-07-10 22:56:59 +0300

    fix style

--- src/gitbok/ui/breadcrumb.clj
+++ src/gitbok/ui/breadcrumb.clj
@@ -9,22 +9,24 @@
     (let [parts (->> (str/split uri #"/")
                      (remove str/blank?)
                      vec)]
-      [:nav {:aria-label "Breadcrumb"}
-       [:ol {:class "flex flex-wrap items-center"}
-        (interpose
-         (ico/chevron-right "chevron size-3 text-gray-400 group-hover:text-primary-9 mx-2")
-         (map-indexed
-          (fn [idx part]
-            (let [path (->> (subvec parts 0 (inc idx)) (str/join "/"))
-                  href (str "/" (gitbok.http/get-prefixed-url context path))
-                  part (str/replace part #"-" " ")]
-              [:li {:key idx
-                    :class "flex items-center gap-1.5"}
-               [:a {:href href
-                    :hx-get (str href "?partial=true")
-                    :hx-target "#content"
-                    :hx-push-url href
-                    :hx-swap "outerHTML"
-                    :class "text-xs font-semibold uppercase items-center gap-1.5 hover:text-tint-strong text-primary-9"}
-                part]]))
-          (vec (drop-last parts))))]])))
+      (when-not (and (= (count parts) 2)
+                     (= (first parts) "readme"))
+        [:nav {:aria-label "Breadcrumb"}
+         [:ol {:class "flex flex-wrap items-center"}
+          (interpose
+           (ico/chevron-right "chevron size-3 text-gray-400 group-hover:text-primary-9 mx-2")
+           (map-indexed
+            (fn [idx part]
+              (let [path (->> (subvec parts 0 (inc idx)) (str/join "/"))
+                    href (str "/" (gitbok.http/get-prefixed-url context path))
+                    part (str/replace part #"-" " ")]
+                [:li {:key idx
+                      :class "flex items-center gap-1.5"}
+                 [:a {:href href
+                      :hx-get (str href "?partial=true")
+                      :hx-target "#content"
+                      :hx-push-url href
+                      :hx-swap "outerHTML"
+                      :class "text-xs font-semibold uppercase items-center gap-1.5 hover:text-tint-strong text-primary-9"}
+                  part]]))
+            (vec (drop-last parts))))]]))))
