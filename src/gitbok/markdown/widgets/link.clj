b8f89666724f036394c543682608c29cbbc17cb0
Svyatoslav Krivosheev <krivosheev.sviat@gmail.com>
2025-07-11 15:39:21 +0300

    fix link

--- src/gitbok/markdown/widgets/link.clj
+++ src/gitbok/markdown/widgets/link.clj
@@ -12,7 +12,9 @@
   (let [href (href context node filepath)
         is-external (str/starts-with? href "http")]
     [:a {:href href
-         :class "text-primary-9 underline underline-offset-2 decoration-1 hover:text-primary-10 hover:decoration-2 transition-all duration-200 inline-flex items-center gap-1"}
+         :class "text-primary-9 underline underline-offset-2
+         decoration-[max(0.07em,1px)] hover:text-primary-12 hover:decoration-primary-9
+         transition-all duration-200 inline-flex items-center gap-1"}
      (or (-> node :content (get 0) :text)
          (-> node :content (get 0)
              :content (get 0) :text))
