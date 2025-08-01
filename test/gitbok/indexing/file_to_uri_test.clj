746597ccc481f7d41dfea104a594d7f94109ed13
Svyatoslav Krivosheev <krivosheev.sviat@gmail.com>
2025-08-01 23:50:11 +0300

    add tests

---
+++ test/gitbok/indexing/file_to_uri_test.clj
@@ -0,0 +1,72 @@
+(ns gitbok.indexing.file-to-uri-test
+  (:require [clojure.test :refer :all]
+            [gitbok.indexing.impl.file-to-uri :as file-to-uri]
+            [gitbok.indexing.impl.summary :as summary]
+            [gitbok.products :as products]
+            [gitbok.constants :as const]))
+
+(defn mock-context []
+  {:current-product-id "test"})
+
+(deftest file->uri-idx-test
+  (testing "External URLs should be filtered out"
+    (with-redefs [summary/read-summary 
+                  (fn [_] 
+                    "# Table of Contents
+* [Getting Started](getting-started.md)
+* [API Reference](api/reference.md)
+* [FHIR Schema reference](https://fhir-schema.github.io/fhir-schema/)
+* [External Docs](https://example.com/docs)
+* [GitHub](https://github.com/aidbox/aidbox)")]
+      (let [result (file-to-uri/file->uri-idx (mock-context))]
+        ;; Should only have internal links
+        (is (= 2 (count result)))
+        (is (contains? result "getting-started.md"))
+        (is (contains? result "api/reference.md"))
+        ;; Should not have external links
+        (is (not (contains? result "https://fhir-schema.github.io/fhir-schema/")))
+        (is (not (contains? result "https://example.com/docs")))
+        (is (not (contains? result "https://github.com/aidbox/aidbox"))))))
+  
+  (testing "Should correctly parse internal links"
+    (with-redefs [summary/read-summary 
+                  (fn [_] 
+                    "* [Home](README.md)
+* [Features](readme/features.md)")]
+      (let [result (file-to-uri/file->uri-idx (mock-context))]
+        (is (= 2 (count result)))
+        ;; Check README.md handling - for root README.md
+        (is (= {:title "Home" :uri "README.md/"} (get result "README.md")))
+        ;; Check regular file
+        (is (= {:title "Features" :uri "readme/features"} 
+               (get result "readme/features.md"))))))
+  
+  (testing "Mixed HTTP and HTTPS links should be filtered"
+    (with-redefs [summary/read-summary 
+                  (fn [_] 
+                    "* [Internal](docs/internal.md)
+* [HTTP Link](http://example.com)
+* [HTTPS Link](https://example.com)
+* [Another Internal](guide.md)")]
+      (let [result (file-to-uri/file->uri-idx (mock-context))]
+        (is (= 2 (count result)))
+        (is (contains? result "docs/internal.md"))
+        (is (contains? result "guide.md"))
+        (is (not (contains? result "http://example.com")))
+        (is (not (contains? result "https://example.com")))))))
+
+(deftest filepath->uri-test
+  (testing "Should return URI for existing filepath"
+    (with-redefs [file-to-uri/get-idx 
+                  (fn [_] {"test.md" {:uri "/test"}})]
+      (is (= "/test" (file-to-uri/filepath->uri (mock-context) "test.md")))))
+  
+  (testing "Should return nil for non-existing filepath"
+    (with-redefs [file-to-uri/get-idx 
+                  (fn [_] {"test.md" {:uri "/test"}})]
+      (is (nil? (file-to-uri/filepath->uri (mock-context) "non-existing.md")))))
+  
+  (testing "Should return nil for external URLs"
+    (with-redefs [file-to-uri/get-idx 
+                  (fn [_] {"test.md" {:uri "/test"}})]
+      (is (nil? (file-to-uri/filepath->uri (mock-context) "https://example.com"))))))
---
+++ test/gitbok/indexing/summary_test.clj
@@ -0,0 +1,79 @@
+(ns gitbok.indexing.summary-test
+  (:require [clojure.test :refer :all]
+            [gitbok.indexing.impl.summary :as summary]
+            [gitbok.products :as products]
+            [gitbok.utils :as utils]
+            [gitbok.http :as http]))
+
+(defn mock-context []
+  {:current-product-id "test"})
+
+(deftest parse-md-link-test
+  (testing "Should parse internal links"
+    (is (= {:title "Getting Started" :href "getting-started"}
+           (summary/parse-md-link "* [Getting Started](getting-started.md)")))
+    (is (= {:title "API Reference" :href "api/reference"}
+           (summary/parse-md-link "* [API Reference](api/reference.md)"))))
+  
+  (testing "Should parse external links"
+    (is (= {:title "FHIR Schema" :href "https://fhir-schema.github.io/fhir-schema/"}
+           (summary/parse-md-link "* [FHIR Schema](https://fhir-schema.github.io/fhir-schema/)")))
+    (is (= {:title "GitHub" :href "https://github.com/aidbox"}
+           (summary/parse-md-link "* [GitHub](https://github.com/aidbox)"))))
+  
+  (testing "Should handle README.md"
+    (is (= {:title "Home" :href ""}
+           (summary/parse-md-link "* [Home](README.md)")))
+    (is (= {:title "Section Home" :href "section"}
+           (summary/parse-md-link "* [Section Home](section/README.md)")))))
+
+(deftest file->href-test
+  (testing "Should preserve external URLs"
+    (is (= "https://example.com" 
+           (summary/file->href "https://example.com")))
+    (is (= "http://example.com" 
+           (summary/file->href "http://example.com"))))
+  
+  (testing "Should transform internal paths"
+    (is (= "getting-started" 
+           (summary/file->href "getting-started.md")))
+    (is (= "api/reference" 
+           (summary/file->href "api/reference.md")))
+    (is (= "" 
+           (summary/file->href "README.md")))
+    (is (= "section" 
+           (summary/file->href "section/README.md")))))
+
+(deftest get-navigation-links-test
+  (testing "Should filter out external links from navigation"
+    (with-redefs [summary/get-summary 
+                  (fn [_] 
+                    [{:parsed {:title "Internal" :href "/docs/internal"}
+                      :href "/docs/internal"}
+                     {:parsed {:title "External" :href "https://example.com"}
+                      :href "https://example.com"}
+                     {:parsed {:title "Another Internal" :href "/docs/another"}
+                      :href "/docs/another"}])]
+      (let [result (summary/get-navigation-links (mock-context))]
+        (is (= 2 (count result)))
+        (is (every? #(not (re-find #"^https?://" (:href %))) result))
+        (is (= "/docs/internal" (:href (first result))))
+        (is (= "/docs/another" (:href (second result)))))))
+  
+  (testing "Should handle empty navigation"
+    (with-redefs [summary/get-summary (fn [_] [])]
+      (let [result (summary/get-navigation-links (mock-context))]
+        (is (= 0 (count result))))))
+  
+  (testing "Should filter various URL schemes"
+    (with-redefs [summary/get-summary 
+                  (fn [_] 
+                    [{:parsed {:title "HTTPS" :href "https://example.com"}
+                      :href "https://example.com"}
+                     {:parsed {:title "HTTP" :href "http://example.com"}
+                      :href "http://example.com"}
+                     {:parsed {:title "Internal" :href "/internal"}
+                      :href "/internal"}])]
+      (let [result (summary/get-navigation-links (mock-context))]
+        (is (= 1 (count result)))
+        (is (= "/internal" (:href (first result))))))))
