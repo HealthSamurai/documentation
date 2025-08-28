7c519d58d6a1750d205b98b9f2184a8c14d999e8
Svyatoslav Krivosheev <krivosheev.sviat@gmail.com>
2025-08-28 22:55:11 +0300

    add every page renders without exception tes

---
+++ test/gitbok/markdown/render_all_test.clj
@@ -0,0 +1,43 @@
+(ns gitbok.markdown.render-all-test
+  (:require
+   [clojure.test :refer [deftest testing is]]
+   [clojure.string :as str]
+   [gitbok.core :as core]
+   [gitbok.products :as products]
+   [gitbok.markdown.core :as markdown]
+   [gitbok.ui.main-content :as main-content]
+   [gitbok.utils :as utils]
+   [gitbok.constants :as const]
+   [gitbok.http :as http]
+   [system]))
+
+(defn read-markdown-file-no-try-catch
+  "Version of read-markdown-file without try-catch block - any exception will fail the test"
+  [context filepath]
+  (println "test render " filepath)
+  (let [[filepath section] (str/split filepath #"#")
+        filepath (products/filepath context filepath)
+        content* (utils/slurp-resource filepath)
+        {:keys [parsed description title]}
+        (markdown/parse-markdown-content
+         context
+         [filepath content*])]
+    {:content
+     (main-content/render-file* context filepath parsed title content*)
+     :title title
+     :description description
+     :section section}))
+
+(deftest test-all-pages-render-without-exceptions
+  (testing "All pages must render without throwing exceptions"
+    (with-redefs [core/read-markdown-file read-markdown-file-no-try-catch
+                  core/dev? false]
+      (let [context {:system (atom {})}
+            _ (http/set-prefix context (or (System/getenv "DOCS_PREFIX") "/docs"))
+            _ (http/set-base-url context (or (System/getenv "BASE_URL") "http://localhost:8081"))
+            _ (http/set-dev-mode context false)
+            products (core/init-products context)]
+        (doseq [product products]
+          (testing (str "Testing product: " (:id product))
+            (core/init-product-indices context product)))
+        (is true "All pages rendered successfully")))))
