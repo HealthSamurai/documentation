697ff3e28c2c9c058549fae033b89043786a2249
Svyatoslav Krivosheev <krivosheev.sviat@gmail.com>
2025-09-04 12:36:49 +0300

    add version query to js and css links to update cache

--- src/gitbok/ui/layout.clj
+++ src/gitbok/ui/layout.clj
@@ -26,117 +26,125 @@
     (main-content/content-div context uri body filepath false hide-breadcrumb)]])
 
 (defn document [context body {:keys [title description canonical-url og-preview lastmod favicon-url section]}]
-  [:html {:lang "en"}
-   [:head
-    (uui/raw "<!-- Google Tag Manager -->")
-    [:script
-     (uui/raw
-      "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  (let [version (gitbok.http/get-version context)
+        version-param (when version (str "?v=" version))]
+    [:html {:lang "en"}
+     [:head
+      (uui/raw "<!-- Google Tag Manager -->")
+      [:script
+       (uui/raw
+        "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PMS5LG2');")]
-    (uui/raw "<!-- End Google Tag Manager -->")
-    [:meta {:charset "utf-8"}]
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-    [:meta {:name "description" :content description}]
-    [:meta {:property "og:title" :content title}]
-    [:meta {:property "og:site_name" :content (:name (products/get-current-product context))}]
-    [:meta {:property "article:author" :content "Health Samurai"}]
-    [:meta {:property "article:modified_time" :content lastmod}]
-    [:meta {:property "article:published_time" :content ""}]
-    [:meta {:property "og:description" :content description}]
-    [:meta {:property "og:url" :content canonical-url}]
-    [:meta {:property "og:type" :content "article"}]
-    [:meta {:property "og:locale" :content "en_US"}]
-    [:meta {:property "og:image" :content og-preview}]
-    [:meta {:property "telegram:channel" :content ""}]
-    [:meta {:property "tg:site_verification" :content "g7j8/rPFXfhyrq5q0QQV7EsYWv4="}]
+      (uui/raw "<!-- End Google Tag Manager -->")
+      [:meta {:charset "utf-8"}]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+      [:meta {:name "description" :content description}]
+      [:meta {:property "og:title" :content title}]
+      [:meta {:property "og:site_name" :content (:name (products/get-current-product context))}]
+      [:meta {:property "article:author" :content "Health Samurai"}]
+      [:meta {:property "article:modified_time" :content lastmod}]
+      [:meta {:property "article:published_time" :content ""}]
+      [:meta {:property "og:description" :content description}]
+      [:meta {:property "og:url" :content canonical-url}]
+      [:meta {:property "og:type" :content "article"}]
+      [:meta {:property "og:locale" :content "en_US"}]
+      [:meta {:property "og:image" :content og-preview}]
+      [:meta {:property "telegram:channel" :content ""}]
+      [:meta {:property "tg:site_verification" :content "g7j8/rPFXfhyrq5q0QQV7EsYWv4="}]
 
-    [:meta {:name "twitter:card" :content "summary_large_image"}]
-    [:meta {:name "twitter:title" :content title}]
-    [:meta {:name "twitter:description" :content description}]
-    [:meta {:name "twitter:image" :content og-preview}]
-    [:meta {:name "twitter:site" :content "@health_samurai"}]
-    [:meta {:name "robots" :content "index, follow"}]
+      [:meta {:name "twitter:card" :content "summary_large_image"}]
+      [:meta {:name "twitter:title" :content title}]
+      [:meta {:name "twitter:description" :content description}]
+      [:meta {:name "twitter:image" :content og-preview}]
+      [:meta {:name "twitter:site" :content "@health_samurai"}]
+      [:meta {:name "robots" :content "index, follow"}]
 
-    [:meta {:name "htmx-config",
-            :content "{\"scrollIntoViewOnBoost\":false,\"scrollBehavior\":\"smooth\",\"allowEval\":false,\"historyCacheSize\":0,\"historyEnabled\":true,\"refreshOnHistoryMiss\":false}"}]
-    (when section
-      [:meta {:name "scroll-to-id" :content section}])
-    [:link {:rel "icon" :type "image/x-icon" :href favicon-url}]
-    [:link {:rel "shortcut icon" :type "image/x-icon" :href favicon-url}]
-    [:link {:rel "apple-touch-icon" :href favicon-url}]
-    [:link {:rel "canonical" :href canonical-url}]
+      [:meta {:name "htmx-config",
+              :content "{\"scrollIntoViewOnBoost\":false,\"scrollBehavior\":\"smooth\",\"allowEval\":false,\"historyCacheSize\":0,\"historyEnabled\":true,\"refreshOnHistoryMiss\":false}"}]
+      (when section
+        [:meta {:name "scroll-to-id" :content section}])
+      [:link {:rel "icon" :type "image/x-icon" :href favicon-url}]
+      [:link {:rel "shortcut icon" :type "image/x-icon" :href favicon-url}]
+      [:link {:rel "apple-touch-icon" :href favicon-url}]
+      [:link {:rel "canonical" :href canonical-url}]
 
-    [:script {:type "application/ld+json"}
-     (uui/raw
-      (json/generate-string
-       {"@context" "https://schema.org"
+      [:script {:type "application/ld+json"}
+       (uui/raw
+        (json/generate-string
+         {"@context" "https://schema.org"
 
-        "@type" "TechArticle"
-        "headline" title
-        "description" description
-        "author" {"@type" "Organization", "name" "HealthSamurai"}}))]
-    [:title (str title " | " (:name (products/get-current-product context)))]
+          "@type" "TechArticle"
+          "headline" title
+          "description" description
+          "author" {"@type" "Organization", "name" "HealthSamurai"}}))]
+      [:title (str title " | " (:name (products/get-current-product context)))]
 
-    [:link {:rel "stylesheet", :href (gitbok.http/get-prefixed-url context "/static/app.min.css")}]
+      [:link {:rel "stylesheet", :href (str (gitbok.http/get-prefixed-url context "/static/app.min.css") version-param)}]
 
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/htmx.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/tabs.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/toc.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/mobile-menu.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/mobile-search.js")}]
-    
-    ;; Syntax highlighting
-    [:link {:rel "stylesheet" :href (gitbok.http/get-prefixed-url context "/static/github.min.css")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/highlight.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/json.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/bash.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/yaml.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/json.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/xml.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/http.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/graphql.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/sql.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/javascript.min.js")}]
-    
-    ;; Mermaid diagrams
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/mermaid.min.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/mermaid-config.js")}]
-    
-    ;; KaTeX for LaTeX math rendering
-    [:link {:rel "stylesheet" :href (gitbok.http/get-prefixed-url context "/static/katex.min.css")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/katex.min.js")}]
-    
-    ;; Meilisearch HTMX keyboard navigation
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/meilisearch-htmx-nav.js")}]
-    
-    ;; Unified content renderer (replaces init-content.js, mermaid-init.js, hljs-init.js)
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/unified-content-renderer.js")}]
-    
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/copy-code.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/heading-links.js")}]
-    [:script {:src (gitbok.http/get-prefixed-url context "/static/lastupdated.js")}]
-    [:script {:src "https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"}]
-    [:script {:defer true
-              :src (gitbok.http/get-prefixed-url context "/static/navigation-click-fix.js")}]
-    [:script {:defer true
-              :src (gitbok.http/get-prefixed-url context "/static/keyboard-navigation.js")}]
-    [:script {:defer true
-              :src (gitbok.http/get-prefixed-url context "/static/posthog.js")}]
-    [:script {:defer true
-              :src (gitbok.http/get-prefixed-url context "/static/scroll-to-id.js")}]]
-   [:body {:hx-boost "true"
-           :hx-on "htmx:afterSwap: window.scrollTo(0, 0);"}
-    (uui/raw "<!-- Google Tag Manager (noscript) -->")
-    [:noscript
-     [:iframe
-      {:src "https://www.googletagmanager.com/ns.html?id=GTM-PMS5LG2" :height "0" :width "0"
-       :style
-       "display:none;visibility:hidden"}]]
-    (uui/raw "<!-- End Google Tag Manager (noscript) -->")
-    body]])
+      ;; not mine
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/htmx.min.js")}]
+      ;; Syntax highlighting
+      [:link {:rel "stylesheet" :href (gitbok.http/get-prefixed-url context "/static/github.min.css")}]
+      [:link {:rel "stylesheet" :href (gitbok.http/get-prefixed-url context "/static/github-dark.min.css")
+              :disabled true}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/highlight.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/json.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/bash.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/yaml.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/json.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/xml.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/http.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/graphql.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/sql.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/javascript.min.js")}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/mermaid.min.js")}]
+      [:link {:rel "stylesheet"
+              :href (gitbok.http/get-prefixed-url context "/static/katex.min.css")
+              :defer true}]
+      [:script {:src (gitbok.http/get-prefixed-url context "/static/katex.min.js")
+                :defer true}]
+
+
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/mermaid-config.js") version-param)}]
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/scroll-to-id.js") version-param)}]
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/tabs.js") version-param)}]
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/toc.js") version-param)}]
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/mobile-menu.js") version-param)}]
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/mobile-search.js") version-param)}]
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/meilisearch-htmx-nav.js") version-param)}]
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/unified-content-renderer.js") version-param)}]
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/heading-links.js") version-param)}]
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/lastupdated.js") version-param) :defer true}]
+
+      [:script {:src (str (gitbok.http/get-prefixed-url context "/static/copy-code.js") version-param)
+                :defer true}]
+      [:script {:defer true
+                :src (str (gitbok.http/get-prefixed-url context "/static/navigation-click-fix.js") version-param)}]
+      [:script {:defer true
+                :src (str (gitbok.http/get-prefixed-url context "/static/keyboard-navigation.js") version-param)}]
+      [:script {:defer true
+                :src (str (gitbok.http/get-prefixed-url context "/static/posthog.js") version-param)}]
+
+      ;; Theme toggle functionality
+      ;; TODO
+      #_[:script {:defer true
+                :src (str (gitbok.http/get-prefixed-url context "/static/theme-toggle.js") version-param)}]
+
+      ]
+
+     [:body {:hx-boost "true"
+             :hx-on "htmx:afterSwap: window.scrollTo(0, 0);"}
+      (uui/raw "<!-- Google Tag Manager (noscript) -->")
+      [:noscript
+       [:iframe
+        {:src "https://www.googletagmanager.com/ns.html?id=GTM-PMS5LG2" :height "0" :width "0"
+         :style
+         "display:none;visibility:hidden"}]]
+      (uui/raw "<!-- End Google Tag Manager (noscript) -->")
+      body]]))
 
 (defn layout [context request
               {:keys [content
