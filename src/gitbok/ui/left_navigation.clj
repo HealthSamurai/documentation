c74f4f37267cdcc3ecd96fefe5d21fb89f82ed6d
Svyatoslav Krivosheev <krivosheev.sviat@gmail.com>
2025-10-08 17:28:36 +0300

    fix left navigation styles

--- src/gitbok/ui/left_navigation.clj
+++ src/gitbok/ui/left_navigation.clj
@@ -18,20 +18,20 @@
         ;; For opening details - check if current URL is under this item's path
         open? (str/starts-with? url (:href item))]
     (if (:children item)
-      [:details.group (when open? {:open ""})
+      [:details.nav-item (when open? {:open ""})
        [:summary
         {:class "flex items-center justify-between
                   text-small font-normal
                   transition-colors duration-200 ease-in-out
-                  cursor-pointer group"}
+                  cursor-pointer"}
         [:div {:class "flex-1 clickable-summary"}
          (add-active-class
           (update item :title
                   (fn [title] (assoc-in title [1 :class] summary/summary-classes)))
           active?)]
-        (ico/chevron-right "chevron size-3 text-small font-normal
+        (ico/chevron-right "nav-chevron size-3 text-small font-normal
                             transition-all
-                            duration-200 transform rotate-0 group-open:rotate-90 mr-4")]
+                            duration-200 mr-9")]
        [:div {:class "ml-6 border-l-1 border-outline"}
         (for [c (:children item)]
           (render-left-navigation url c))]]
@@ -45,10 +45,10 @@
     lg:-ms-4"
     :aria-label "Documentation menu"}
    (for [item summary]
-     [:div {:class "break-words"}
+     [:div {:class "break-words mt-4 border-t border-outline-subtle pt-4 first:mt-0 first:border-t-0 first:pt-0"}
       (when-not
        (str/blank? (:title item))
-        [:div {:class "mt-4 mb-1 first:mt-2 ml-4"}
+        [:div {:class "mb-1 first:mt-2 ml-4"}
          [:span {:class "text-xs font-medium leading-4 text-brand uppercase tracking-wider"}
           (:title item)]])
       (for [ch (:children item)]
